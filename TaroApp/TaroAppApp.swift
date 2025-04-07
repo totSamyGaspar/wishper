15de17f43b354571c80b2afca6a0b9b409654feb
Edward Gasparian <gilberd@i.ua>
2025-04-07 12:43:39 +0400

    Initial Commit

---
+++ TaroApp/TaroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaroAppApp.swift
+//  TaroApp
+//
+//  Created by Edward Gasparian on 07.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct TaroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
